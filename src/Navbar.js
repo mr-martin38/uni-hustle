3cadc49ae5e4292ac6574a7ef8a38c1fbdb04d74
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-05-02 23:51:23 +0100

    fourth commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = () => {
         <img className="site-logo" src="/images/uni-logo.png" />
         <h1 className="site-name">UniHustle</h1>
         <nav className="search-bar">
-          {" "}
           <input type="text" placeholder="Search for Products, services and Trainings" />
         </nav>
         <span>Products</span>
@@ -13,6 +12,7 @@ const Navbar = () => {
         <span>Trainings</span>
         <span>Contact Us</span>
       </nav>
+      <img className="eyes" src="/images/eyes.png" />
     </nav>
   );
 };
