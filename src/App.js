4c16f32effeceb3498a7a633a46856ee4cc16c19
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-05-03 17:53:00 +0100

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Business from "./Business";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />;
       <Business />
+      <Card />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div className="product-frame">
+      <div className="product-card">
+        <img className="shoe" src="/images/shoe.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
