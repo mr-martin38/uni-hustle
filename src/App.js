3348a59dfca0503ab87a71c100a41c829ba10caa
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-05-03 02:09:23 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Business from "./Business";
 
 function App() {
   return (
     <div className="App">
       <Navbar />;
+      <Business />
     </div>
   );
 }
---
+++ src/Business.js
@@ -0,0 +1,9 @@
+const Business = () => {
+  return (
+    <nav className="business-card">
+      <img className="business-logo" src="/images/uni-logo.png" />
+    </nav>
+  );
+};
+
+export default Business;
