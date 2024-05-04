0fee659608775d62487762d577024cc4dd8d08ba
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-05-04 23:58:22 +0100

    eight commit

--- src/Card.js
+++ src/Card.js
@@ -3,6 +3,11 @@ const Card = () => {
     <div className="product-frame">
       <div className="product-card">
         <img className="shoe" src="/images/shoe.png" />
+        <p className="store-name">Sarah Closet Stores</p>
+        <h3 className="shoe-desc">Nike Air Max 270 Series</h3>
+        <p className="shoe-info">The new Nike air max 270 series</p>
+        <img src="/images/star.png" className="star" />
+        <p className="price">$400.00</p>
       </div>
     </div>
   );
